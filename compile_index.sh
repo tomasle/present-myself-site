d286bdac2514d7037d6687ca7f7a27868fb91975
Tomas Levin <tomas.levin@cacanny.net>
2021-12-22 11:48:32 +0100

    Update compile_index.sh

--- compile_index.sh
+++ compile_index.sh
@@ -2,7 +2,7 @@ JUPYTER_FOLDER=${1?Must provide first argument as output folder}
 BASE_DOCUMENT=${2?Must provide first argument as output folder}
 OUTPUT_PATH=${3}
 
-cat $BASE_DOCUMENT > oversikt_notebook.md
+cat $BASE_DOCUMENT > index.md
 
 mkdir -p $JUPYTER_FOLDER
 rm $JUPYTER_FOLDER/*.ipynb
@@ -20,5 +20,5 @@ jupyter nbconvert --to html --output-dir $OUTPUT_PATH/ $JUPYTER_FOLDER/*.ipynb
 for notebook in $OUTPUT_PATH/*.html
 
 do
-  echo "$notebook" | tee -a oversikt_notebook.md
+  echo "$notebook" | tee -a index.md
 done
